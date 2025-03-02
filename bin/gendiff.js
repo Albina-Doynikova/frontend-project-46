a285152dd499e4fc8efe457088ee9907adcfe08b
Albina-Doynikova <informa601@gmail.com>
2025-03-02 10:53:51 +0300

    update

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,16 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import genDiff from '../src/index.js';
 
 const program = new Command();
 
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+
 const gendiff = program
     .name('gendiff')
-    .description('Compares two configuration files and shows a difference.')
     .option('-f, --format', 'type format')
     .argument('<filepath1>')
     .argument('<filepath2>')
