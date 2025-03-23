5833bd7be0eec6d05026a900910f66287ee135ac
Albina-Doynikova <informa601@gmail.com>
2025-03-23 11:04:37 +0300

    update

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,13 +1,12 @@
 import genDiff from '../src/index.js';
 import path from 'path';
-import fs from 'fs';import { expect, test } from '@jest/globals';
+import fs from 'fs';
+import { expect, test } from '@jest/globals';
 
-const getFixturesPath = (filename) => path.resolve(process.cwd(), filename);
+const getFixturesPath = (filename) => path.resolve(process.cwd(), '__fixtures__', filename);
 const result = fs.readFileSync(getFixturesPath('result.txt'), 'utf-8');
 test('genDiff', () => {
     expect(
         genDiff(getFixturesPath('file1.json'),getFixturesPath('file2.json'))
     ).toBe(result);
 });
-
-
---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+
+export default [
+  pluginJs.configs.recommended,
+  {
+    files: [
+      '**/*.{js}',
+    ],
+  },
+  {
+    ignores: ['dist/'],
+  },
+  {
+    languageOptions: {
+      globals: globals.node,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      'no-underscore-dangle': ['error', { allow: ['__filename', '__dirname'] }],
+      'no-prototype-builtins': 'off', 
+    },
+  },
+];
