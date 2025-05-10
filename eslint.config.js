2ff1295410a8d77d12c58faf1019cf91802bb86e
Albina-Doynikova <informa601@gmail.com>
2025-05-11 02:28:27 +0300

    update

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,31 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+import pluginJest from 'eslint-plugin-jest';
 
 export default [
+  stylistic.configs.recommended,
   pluginJs.configs.recommended,
+
   {
-    ignores: ['dist/'],
-  },
-  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: {
+      js: pluginJs,
+    },
+    extends: ['js/recommended'],
     languageOptions: {
       globals: globals.node,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-      },
     },
+  },
+
+  {
+    languageOptions: {
+      globals: pluginJest.environments.globals.globals,
+    },
+  },
+
+  {
+    ignores: ['dist/'],
     rules: {
       'no-underscore-dangle': ['error', { allow: ['__filename', '__dirname'] }],
       'no-prototype-builtins': 'off',
